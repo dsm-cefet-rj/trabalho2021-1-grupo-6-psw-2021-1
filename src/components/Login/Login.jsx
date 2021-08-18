4bd14737cea954213cabf31fb1b7399d1e3c89da
Eduardo Tadashi <quadros.tadashi@gmail.com>
2021-08-18 01:05:11 -0300

    tabulation Login.jsx

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -13,19 +13,18 @@ export default class Login extends Component {
                                 <form id="login-form" class="form" action="" method="post">
                                     <h3 class="text-center text-info">Login</h3>
                                     <div class="form-group">
-                                        <label for="username" class="text-info">Username:</label><br></br>
+                                        <label for="username" class="text-info">Username:</label><br />
                                         <input type="text" name="username" id="username" class="form-control"/>
                                     </div>
                                     <div class="form-group">
-                                        <label for="password" class="text-info">Password:</label><br></br>
+                                        <label for="password" class="text-info">Password:</label><br /> 
                                         <input type="text" name="password" id="password" class="form-control"/>
                                     </div>
                                     <div class="form-group">
-                                        <label for="remember-me" class="text-info"><span>Remember me</span> <span><input
-                                            id="remember-me" name="remember-me" type="checkbox"/></span></label><br></br>
+                                        <label for="remember-me" class="text-info"><span>Remember me</span><span><input id="remember-me" name="remember-me" type="checkbox"/></span></label><br/>
                                         <input type="submit" name="submit" class="btn btn-info btn-md" value="submit"/>
                                     </div>
-                                    <div id="register-link" class="text-right">
+                                    <div id="register-link" class="text-right"> 
                                         <a href="#" class="text-info">Register here</a>
                                     </div>
                                 </form>
