9d985a025e9b3955f142f4fbc895cf2125ebad51
Eduardo Tadashi <quadros.tadashi@gmail.com>
2021-08-18 12:18:58 -0300

    component HomePsic

--- src/components/HomePsic/HomePsic.jsx
+++ src/components/HomePsic/HomePsic.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import ButtonSupport from '../utils/ButtonSupport';
 
 import PacientesList from "../PacientesList";
 import PsicForm from "../PsicForm";
@@ -27,11 +27,7 @@ export default class HomePsic extends Component {
         <div class="container-sm">
         <p class="h1 text-center">OnPsic</p>
     
-        <div class="d-flex align-items-start">
-          <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-            <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" type="button" role="tab" aria-selected="false">Suporte Humanizado</button>
-          </div>
-        </div>
+        <ButtonSupport titulo="Botões de suporte" />
     
         <div class="list-group">
           <h3>Meus Serviços</h3>
