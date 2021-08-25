c3459adc572dc9e15e63e916de92afefc7710996
Eduardo Tadashi <quadros.tadashi@gmail.com>
2021-08-25 18:47:38 -0300

    maintance ConsultasForm

--- src/components/Consultas/ConsultasForm.jsx
+++ src/components/Consultas/ConsultasForm.jsx
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { useHistory } from 'react-router-dom';
-
 export default class PsicForm extends Component{
     
     
-=======
 
-export default class PsicForm extends Component{
->>>>>>> 43bc8c2b07eebeacd83c90d15684807b51c1b2af
-=======
 
-export default class PsicForm extends Component{
->>>>>>> 43bc8c2b07eebeacd83c90d15684807b51c1b2af
+
+
+
+
     _createCard(event){
         event.preventDefault();
         event.stopPropagation();
